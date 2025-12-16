a805707c7a7d8d87a99929da69dd61d751b81d8b
dandeeccastro <dundeepessoal@gmail.com>
2025-12-16 14:58:03 -0300

    WIP

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import threading
 
 
 def load_dotenv():
+    """Loads main_dir from .env file"""
     with open(".env") as env:
         data = env.read()
         target_dir = data.split("=")[1]
@@ -72,7 +73,7 @@ class Node:
     @classmethod
     def get_filetype(cls, filename):
         if len(filename.split(".")) > 1:
-            return filename.split(".")[1]
+            return filename.split(".")[-1]
         return "Folder"
 
     @classmethod
@@ -106,26 +107,34 @@ class Node:
         return "{}\n".format(csvl)
 
 
-def generate_graph_csv_from_path(path):
-    main_file = os.path.basename(path)
-    generate_graph(path)
+def write_graph_to_csv(filename, root_node: Node):
+    """Writes graph to csv file
 
-    with open("{}.csv".format(main_file), "w") as file:
-        for node in graph:
-            print(node.uuid, node.parent)
-            file.write(node.print_csv_line())
-
-
-def write_graph_to_csv(filename):
-    print(filename)
+    Keyword arguments:
+    filename -- the name of the file to be created
+    """
     with open("{}.csv".format(filename), "w") as file:
         file.write(Node.print_csv_header())
-        for node in graph.values():
-            print(node.uuid, node.parent)
+        nodes = [root_node]
+        while len(nodes) > 0:
+            node = nodes.pop(0)
             file.write(node.print_csv_line())
+            nodes += node.children
+
+    # with open("{}.csv".format(filename), "w") as file:
+    #     file.write(Node.print_csv_header())
+    #     for node in graph.values():
+    #         print(node.uuid, node.parent)
+    #         file.write(node.print_csv_line())
 
 
 def build_graph_from_csv(csvfile):
+    """Builds a node graph from CSV graph data.
+    Returns root node of said graph
+
+    Keyword Arguments:
+    csvfile -- csv file that contains graph data
+    """
     root = None
     graph = {}
     with open(csvfile) as f:
@@ -157,10 +166,18 @@ def build_graph_from_csv(csvfile):
 
 
 def generate_graph(path, parent=None, recursive=True, should_calculate=True):
+    """Recursively generates a graph based on a filepath.
+    Returns root node for said graph.
+
+    Keyword Arguments:
+    path -- path of folder that should be analyzed
+    parent -- parent of current node
+    recursive -- defines wether we should recurse into filesystem or not
+    should_calculate -- defines wether we should calculate file hash or not
+    """
     node = Node.from_filetree(
         parent=parent, children=[], path=path, should_calculate_hash=should_calculate
     )
-    graph[node.uuid] = node
 
     if parent:
         parent.children.append(node)
@@ -172,72 +189,88 @@ def generate_graph(path, parent=None, recursive=True, should_calculate=True):
         return node
 
     entries = os.scandir(path)
-    children = []
     for entry in entries:
-        children.append(
-            generate_graph(entry.path, parent=node, should_calculate=should_calculate)
-        )
+        generate_graph(entry.path, parent=node, should_calculate=should_calculate)
 
-    graph[node.uuid].children = children
-
-    joint_hashes = ",".join(map(lambda x: x.hash, children)).encode("utf-8")
-    graph[node.uuid].hash = hashlib.sha256(joint_hashes).hexdigest()
+    joint_hashes = ",".join(map(lambda x: x.hash, node.children)).encode("utf-8")
+    node.hash = hashlib.sha256(joint_hashes).hexdigest()
 
     return node
 
 
-def check_for_duplicates():
+def check_for_duplicates(root):
+    """Prints duplicate files inside graph"""
     hashycheky = {}
-    for node in graph.values():
+    nodes = [root]
+    while len(nodes) > 0:
+        node = nodes.pop(0)
         if node.hash in hashycheky:
             hashycheky[node.hash].append(node)
         else:
             hashycheky[node.hash] = [node]
+        nodes += node.children
 
     for nodes in hashycheky.values():
         if len(nodes) > 1:
-            log_duplicate(nodes[0], nodes)
-
-
-def log_duplicate(curr, dups):
-    print("Arquivo {} é uma duplicata!".format(curr.path))
-    print("Duplicatas:")
-    for dup in dups:
-        print("\tDuplicata: {}".format(dup.path))
+            print("Arquivo {} é uma duplicata!".format(nodes[0].path))
+            print("Duplicatas:")
+            for node in nodes:
+                print("\tDuplicata: {}".format(node.path))
 
 
 def build_graph_from_filesystem(main_dir, should_calculate=True):
+    """Builds a graph starting from given directory
+    Returns root node of said graph
+
+    Keyword arguments:
+    main_dir -- Starting directory for graph generation
+    should_calculate -- Defines wether we should calculate file hashes during traversal
+    """
     root_node = generate_graph(
-        main_dir, recursive=True, should_calculate=should_calculate
+        main_dir, recursive=False, should_calculate=should_calculate
     )
 
-    # threads = []
-    # for entry in os.scandir(main_dir):
-    #     t = threading.Thread(
-    #         target=generate_graph,
-    #         args=(entry.path,),
-    #         kwargs={"parent": root_node, "should_calculate": should_calculate},
-    #     )
-    #     threads.append(t)
+    threads = []
+    for entry in os.scandir(main_dir):
+        t = threading.Thread(
+            target=generate_graph,
+            args=(entry.path,),
+            kwargs={"parent": root_node, "should_calculate": should_calculate},
+        )
+        threads.append(t)
 
-    # for t in threads:
-    #     t.start()
-    # for t in threads:
-    #     t.join()
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
 
     return root_node
 
 
-def print_graph():
+def print_graph(root):
+    """Prints graph for debugging"""
+
     print("NORMAL")
-    for node in graph.values():
+    nodes = [root]
+    while len(nodes) > 0:
+        node = nodes.pop(0)
         print("{}\t{}".format(node.name, node.hash))
+        nodes += node.children
+
     print("\nDEBUG")
-    for node in graph.values():
+    nodes = [root]
+    while len(nodes) > 0:
+        node = nodes.pop(0)
         print("\t".join([node.name, str(node.parent), node.hash]))
+        nodes += node.children
 
 
 def check_correctedness(main_dir):
+    """Checks wether graph built from csv and through code are the same
+
+    Keywords Arguments:
+    main_dir -- Directory that should be analyzed
+    """
     curr_dir = os.path.basename(main_dir)
     root_node_csv = build_graph_from_csv("{}.csv".format(curr_dir))
     root_node_graph = build_graph_from_filesystem(main_dir, should_calculate=False)
@@ -245,6 +278,7 @@ def check_correctedness(main_dir):
 
 
 def are_trees_equal(node_a, node_b):
+    """Evaluates if node_a and node_b are equal and returns the result."""
     if not node_a and not node_b:
         return True
 
@@ -269,19 +303,15 @@ def are_trees_equal(node_a, node_b):
     return result
 
 
-graph = {}
-
-
 if __name__ == "__main__":
     main_dir = load_dotenv()
     if len(sys.argv) > 1 and sys.argv[1] == "-c":
         are_equal = check_correctedness(main_dir)
         print("Are files equal? {}".format(are_equal))
     elif len(sys.argv) > 1 and sys.argv[1] == "-d":
-        build_graph_from_filesystem(main_dir, should_calculate=False)
-        write_graph_to_csv(os.path.basename(main_dir))
+        root = build_graph_from_filesystem(main_dir, should_calculate=False)
+        write_graph_to_csv(os.path.basename(main_dir), root)
     else:
-        build_graph_from_filesystem(main_dir)
-        print(main_dir)
-        write_graph_to_csv(os.path.basename(main_dir))
-        check_for_duplicates()
+        root = build_graph_from_filesystem(main_dir)
+        write_graph_to_csv(os.path.basename(main_dir), root)
+        check_for_duplicates(root)
